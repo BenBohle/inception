ab10982fa2d2e0fb352881f74921489c70f7f603
Ben Bohle <bbohle@3-D-6.42heilbronn.de>
2025-01-27 19:51:02 +0100

    mhmhmhm

--- Dockerfiles/mariadb/docker-entrypoint.sh
+++ Dockerfiles/mariadb/docker-entrypoint.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-# Starte MariaDB
+service mariadb start
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME};"
+mysql -u root -e "CREATE USER IF NOT EXISTS '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+service mariadb stop
+
+# Starte den MariaDB-Dienst im Vordergrund
 exec "$@"
