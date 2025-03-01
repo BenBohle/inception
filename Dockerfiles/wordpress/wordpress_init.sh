6da4ae94e9dadaa7848fdd84d0c538e28d747f1e
Ben Bohle <bbohle@1-A-12.42heilbronn.de>
2025-03-01 14:46:05 +0100

    nefijsfdilsfjs

--- Dockerfiles/wordpress/wordpress_init.sh
+++ Dockerfiles/wordpress/wordpress_init.sh
@@ -24,7 +24,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
 # WordPress installieren, falls es noch nicht installiert ist
   echo "Installiere WordPress..."
-  wp core install --url="${DOMAIN_NAME}" --title="WordPress" \
+  wp core install --url="https://${DOMAIN_NAME}" --title="WordPress" \
     --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
     --admin_email="${WORDPRESS_ADMIN_EMAIL}" --skip-email --allow-root
 
