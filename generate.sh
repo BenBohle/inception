7677ed40d83cb273fb085a04577253e36390c47c
Ben Bohle <bbohle@1-A-12.42heilbronn.de>
2025-03-03 17:32:28 +0100

    achhh

---
+++ generate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "test1" > wp_db_user.txt
+echo "test2" > wp_db_password.txt
+echo "test3" > db_root_password.txt
+echo "test4" > wp_admin_email.txt
+echo "test5" > wp_admin.txt
+echo "test6" > wp_admin_password.txt
+echo "test7" > wp_user.txt
+echo "test8" > wp_user_password.txt
+echo "test9" > wp_user_email.txt
